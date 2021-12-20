dadc98970022c9b06744208c6b65d077ce8a1bfc
Shirllie-Apiyo <apraise320@gmail.com>
2021-12-20 19:43:25 +0300

    settimeout asychronous javascript

---
+++ timeout.js
@@ -0,0 +1,14 @@
+// say Hello
+console.log("Hello.");
+// say goodbye two seconds from now.
+setTimeout(function(){
+    console.log("Goodbye!");
+},2000);
+//say "Hello again!"
+console.log("Hello again!");
+
+// the above code will 
+// say "Hello"
+//Say "Hello again!"
+//do nothing for two seconds
+//say "Goodbye!"
